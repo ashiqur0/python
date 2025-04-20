bfbd862b5e593869460d6c94c364e83190431445
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-20 14:08:57 +0600

    Python Syntax Studied

--- Python.py
+++ Python.py
@@ -1,4 +1,15 @@
 #### Structured way to Learn Python ####
 
-# First Program in Python
-print("Hello, World!")
+# # First Program in Python
+# print("Hello, World!")
+
+#### Python Syntax ####
+# Python Indentation 
+if 5 > 2:
+    print("Five is greater than two")
+
+# Python Variables
+x = 5
+y = "Hello World!"
+
+# Comments
