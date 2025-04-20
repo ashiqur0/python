335c6bc1451f383b71c00f6f474d77df00f72e5f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-20 16:57:30 +0600

    Python Comments Studied

---
+++ 3_PythonComments.py
@@ -0,0 +1,18 @@
+# Python Commments
+
+# This is a single line comments in python
+print("Hello")
+
+"""
+This is a multiline 
+commnets
+in Python
+"""
+print("Hello 2")
+
+'''
+This is also a
+multiline comments
+in Python
+'''
+print("Hello 3")
