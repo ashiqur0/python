cdd1bc111b406883e9825d7796f4a04c326c3409
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-20 16:53:04 +0600

    Python Syntax Studied

---
+++ 2_PythonSyntax.py
@@ -0,0 +1,17 @@
+# Python Indentation
+if 5 > 3:
+    print("Five is greater than three")
+    
+# if 5 > 3:
+# print("Five is greater than three")   # Syntax Error Due to Indentation
+
+if 4 > 3:
+    print("Four is greater than three")
+        # print("No error")             # Syntax Error (Scope element must be in same indentation)
+        
+# Python Variables
+x = 5
+y = "John"
+
+# Python Comments
+print("Hello, World!")
