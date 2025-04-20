600e38c811aa3abab641bd5eecc51ffa4797533d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-20 14:13:53 +0600

    Python Comments Studied

--- Python.py
+++ Python.py
@@ -3,13 +3,25 @@
 # # First Program in Python
 # print("Hello, World!")
 
-#### Python Syntax ####
-# Python Indentation 
-if 5 > 2:
-    print("Five is greater than two")
+# #### Python Syntax ####
+# # Python Indentation 
+# if 5 > 2:
+#     print("Five is greater than two")
 
-# Python Variables
-x = 5
-y = "Hello World!"
+# # Python Variables
+# x = 5
+# y = "Hello World!"
 
-# Comments
+# # Comments
+
+#### Python Comments ####
+# This is a comments
+"""
+This is a multiline
+comments
+"""
+
+'''
+this is also a 
+multiline comments
+'''
