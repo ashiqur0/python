ce6529644ea7aa67400540d469fbe6e7d557f7ae
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-21 06:17:59 +0600

    Assining Multiple Values

---
+++ 4_2_AssignMultipleValues.py
@@ -0,0 +1,11 @@
+#### Assign Multiple Values ####
+
+# Many Value to Multiple Variables
+x, y, z = "apple", "banana", "cherry"
+
+# One value to Multiple variable
+x = y = z = "apple"
+
+# Unpack Collection
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
