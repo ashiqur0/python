392fbfbfe336c7087e7940b84b47896b83a505a3
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-21 06:15:14 +0600

    Python Variable Namming is Studied

---
+++ 4_1_PythonVariableNames.py
@@ -0,0 +1,18 @@
+### Python Variable Names ####
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Illegal Variable
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+# Multi Words Variable Names
+myVariableNames = "John"    # Camel Case
+MyVariableNames = "John"    # Pascal Case
+my_variable_names = "John"  # Snake Case
