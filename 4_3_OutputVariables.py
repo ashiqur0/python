a2dce7b00d316c1158b9e1ed46196d1bc4202144
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-21 06:19:20 +0600

    Python Output Variables Studied

---
+++ 4_3_OutputVariables.py
@@ -0,0 +1,29 @@
+#### Output Variables ####
+
+x = "Python is awesome"
+print(x)
+
+# Output multiple variable separated by comma
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+# + operator to output multiple variable
+x = "python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+# For numbers '+' operators works as mathmatical operator
+x = 5
+y = 6
+print(x + y)    # 11
+
+# Number and String can not execute in print() using '+' operator
+x = 5
+y = "John"
+# print(x + y) # Error
+
+# But it is possible by using ',' comma
+print(x, y)
