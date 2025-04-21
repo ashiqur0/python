1c0b7a1f9009fcd999266216651beaca1717680f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-21 06:13:07 +0600

    Python Variables Studied

---
+++ 4_0_PythonVariables.py
@@ -0,0 +1,22 @@
+########## Python Variables ##########
+
+# Creating Variables 
+x = 5
+y = "John"
+print(x)
+
+# Type Casting 
+x = str(3)
+y = int("3")
+z = float(3)
+
+# Get the Type
+print(type(x))  # String
+
+# Single or Double Quote is the same
+x = "John"
+x = 'John'
+
+# Case-Sensitive 
+a = "John"
+A = 5   # A will not override a
