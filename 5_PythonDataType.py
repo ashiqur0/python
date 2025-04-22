ed181802756b4d7d70639f6f431a18494f9506c3
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-22 08:04:27 +0600

    Python Data Type

---
+++ 5_PythonDataType.py
@@ -0,0 +1,55 @@
+####################  Python Data Type ####################
+
+#### Text Type ####
+# str
+x = "Python is awesome"
+
+#### Numeric Type ####
+# int
+x = 5
+
+# float
+x = 45.3
+
+# complex
+x = 3j
+
+#### Sequence Type ####
+# List
+fruits = ["apple", "banana", "cherry"]
+
+# Tuple
+car = ("ford", "toyota", "marcedis")
+
+# Range
+x = range(5)
+
+#### Mapping Type ####
+# dict
+student = {"name":"John", "roll":30}
+
+#### Set Type ####
+# set 
+fruits = {"apple", "banana", "cherry"}
+
+# frozenset 
+x = frozenset({"apple", "banana", "cherry"})
+
+#### Bool Type ####
+# bool 
+x = True
+y = False
+
+#### Binary Types ####
+# bytes 
+x = b"Hello"
+
+# bytearray
+x = bytearray(5)
+
+# memoryview
+x = memoryview(bytes(5))
+
+
+#### None Type ####
+x = None
