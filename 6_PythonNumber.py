bb54b35888712d20700addeb7f5ad28bcd6e0547
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-23 09:22:34 +0600

    Python Numbers

---
+++ 6_PythonNumber.py
@@ -0,0 +1,17 @@
+################ Python Numbers ################
+
+x = 3       # int
+y = 5.3     # float
+z = 3j      # complex
+
+# get the type 
+print(type(x))
+
+# Type Conversion
+a = float(x)
+b = int(y)
+c = str(z)
+d = complex(x)
+
+print(type(d))
+print(d)    # 3 + 0j
