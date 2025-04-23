b72912b03d19177770f5e6bf1dd61552612d9b7f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-23 19:32:05 +0600

    Python List

---
+++ 11_0_List.py
@@ -0,0 +1,25 @@
+################ Python List ################
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+# Allow Duplicates
+fruits = ["apple", "banana", "cherry", "apple", "cherry"]
+print(fruits)
+
+# List Length
+print(len(fruits))
+
+# List Items- Data Type
+list1 = ["apple", "banana", "cherry"]
+list2 = [3, 4, 2, 5, 1, 6]
+list3 = [True, False, True, False]
+
+# A list with string, integers, boolean value
+list4 = ["apple", 3, True]
+
+# Get the Type
+print(type(list4))
+
+# The list() constructor used to create a list
+fruits = list(("apple", "banana", "cherry"))
+print(fruits)
