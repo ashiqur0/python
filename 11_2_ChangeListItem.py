285819e8859386ffb93745df0b41dedfefbf20e3
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-24 02:46:45 +0600

    Change List Item

---
+++ 11_2_ChangeListItem.py
@@ -0,0 +1,27 @@
+#### Change List Item ####
+# Change Item Value
+fruits = ['apple', 'banana', 'cherry']
+
+# Change Second Item
+fruits[1] = 'blackcurrant'
+print(fruits)
+
+# Change Range of Item values
+fruits = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'mango']
+fruits[1:3] = ['blackcurrant', 'watermelon']
+print(fruits)
+
+# Change Second value by Replacing it with two new value
+fruits = ['apple', 'banana', 'cherry']
+fruits[1:2] = ['blackcurrant', 'watermelon']
+print(fruits)
+
+# Change second and third value by replacing with one value
+fruits[1:3] = ['banana']
+print(fruits)
+
+# Insert Items
+fruits = ['apple', 'banana', 'cherry']
+fruits.insert(2, 'orange')
+print(fruits)
+
