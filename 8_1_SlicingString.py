f19056550dbfcb00603fd57c1f50a4a852223266
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-23 09:57:18 +0600

    Slicing String

---
+++ 8_1_SlicingString.py
@@ -0,0 +1,14 @@
+################ Slicing String ################
+
+# Get the character from 2 to 5 not include
+a = 'Hello World'
+print(a[2:5])
+
+# Slice from start
+print(a[:5])
+
+# Slice to end
+print(a[2:])
+
+# Negative Indexing
+print(a[-5:-2])
