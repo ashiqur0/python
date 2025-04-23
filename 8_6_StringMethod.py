592b00318c293e11a0d9921cd69e2258eca46bfa
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-23 13:26:38 +0600

    String Methods

---
+++ 8_6_StringMethod.py
@@ -0,0 +1,20 @@
+################ String Methods ################
+txt = "this is A plain txt for testing strign methods"
+print(txt.capitalize())         # Convert First Character to upper case
+print(txt.casefold())           # Convert String into lower case
+print(txt.endswith("methods"))
+print(txt.isalnum())            # Returns True if all characters in the string are alphanumeric
+print(txt.isalpha())
+print(txt.isascii())
+print(txt.isdecimal())
+print(txt.isdigit())
+print(txt.istitle())
+print(txt.isupper())
+print(txt.lower())              # Convert String into lower case
+print(txt.replace("is", "1"))   # 
+print(txt.startswith("this"))
+print(txt.strip())
+print(txt.split(" "))
+print(txt.title())
+print(txt.upper())
+print(txt.find("is"))           # Return the position
