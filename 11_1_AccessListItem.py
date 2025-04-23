929a5437119e93fb9c5391116c4c1bfed1802aa9
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-23 19:42:09 +0600

    Access List Item

---
+++ 11_1_AccessListItem.py
@@ -0,0 +1,24 @@
+#### Access List Item ####
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])
+
+# Negative Index
+print(fruits[-1])
+
+# Range of Indexes
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+# Access Item from beginning to
+print(fruits[:5])
+
+# Access item from 'cherry' to end
+print(fruits[2:])
+
+# Range of Negative Index
+print(fruits[-5:-2])
+
+# Check if Item exist in the list
+if "cherry" in fruits:
+    print('Yes. cherry in the list')
+
