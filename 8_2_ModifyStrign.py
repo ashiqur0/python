ac90586b004e58c7fc189575dd5a431207dda1ba
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-23 10:04:55 +0600

    Modigy String

---
+++ 8_2_ModifyStrign.py
@@ -0,0 +1,22 @@
+################ Modify String ################
+
+# Upper Case
+a = "Hello World"
+print(a.upper())
+
+# Lower Case
+print(a.lower())
+
+# Remove Whitespace
+a = "   Hello World!    "
+print(a.strip())        # returns 'Hello World!'
+
+# Replacing Strign
+a = "Hello World!"
+print(a.replace("H", "J"))
+
+# Split Strign
+a = "Hello, World!"
+b = a.split(",")    # b = ['Hello', "World!"]
+print(b)
+
