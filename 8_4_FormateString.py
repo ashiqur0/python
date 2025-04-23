760e4a2374d30044bd05a89e9eb0f342a16e948c
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-23 12:31:30 +0600

    Format String

---
+++ 8_4_FormateString.py
@@ -0,0 +1,15 @@
+################ Format String ################
+
+# F-String 
+age = 36
+txt = f"My name is John. I am {age}"
+print(txt)
+
+# Placeholder and Modifiers
+price = 99
+txt = f"The price is {price:.2f} dollars"   # Display the price with 2 decimal
+print(txt)
+
+# Perform math operation in the placeholder and return the result
+txt = f"The price is {20*3} dollars"
+print(txt)
