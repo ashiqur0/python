92e4d52f60fff620e12ad2e7df9b543a476f6295
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-23 12:45:21 +0600

    Scape Sequence

---
+++ 8_5_EscapeCharacter.py
@@ -0,0 +1,9 @@
+################ Python Escape Character ################
+print("\'")             # Single Quote
+print("\\")             # Double Quote
+print('\n')             # New line
+print("Hello\rWorld")   # Carriage Return
+print("a\tf")           # Tab
+print("Hello \bWorld")  # Backspace
+print("\110\145\154\154\157")   # Octal value
+print("\x48\x65\x6c\x6c\x6f")   # Hex value
