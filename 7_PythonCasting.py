34ce6b5ca59d42a87b7911fe6ac6e7a5920e4002
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-23 09:31:23 +0600

    Python Casting

---
+++ 7_PythonCasting.py
@@ -0,0 +1,16 @@
+################ Python Casting ################
+# Integers 
+x = int(1)      # Will be 1
+y = int(2.8)    # Will be 2
+z = int("3")    # Will be 3
+
+# Float 
+w = float(1)    # Will be 1.0
+x = float(2.8)  # Will be 2.8
+y = float("3")  # Will be 3.0
+z = float("4.2")# Will be 4.2
+
+# Strings
+x = str("s1")   # Will be 's1'
+y = str(2)      # Will be '2'
+z = str(3.0)    # Will be '3.0'
