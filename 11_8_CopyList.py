5d02a08738aa2f465d726db3a707f20f5a4f1887
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-24 09:29:30 +0600

    Copy List

---
+++ 11_8_CopyList.py
@@ -0,0 +1,17 @@
+#### Copy List ####
+
+# Use the copy() method
+fruits = ['apple', 'banana', 'cherry']
+newlist = fruits.copy()
+print(newlist)
+
+# Use the list()
+num = [10, 89, 30, 50, 20]
+newnum = list(num)
+print(num)
+
+# Use the slice operator
+price = [20.8, 49.9, 99,99, 11.9]
+newprice = price[:]
+print(newprice)
+
