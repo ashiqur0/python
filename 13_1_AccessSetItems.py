46a47fd4e099744e8f0523c7493ac74b930c175a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-25 15:08:18 +0600

    Access Set Item

---
+++ 13_1_AccessSetItems.py
@@ -0,0 +1,17 @@
+#### Python Access Set Items ####
+
+# Access Item : is not possible in set but
+fruitSet = {'apple', 'banana', 'cherry'}
+print(fruitSet)
+
+# Can access through for loop
+for x in fruitSet:
+    print(x)
+
+# Can check if an item is present in a set
+print('apple' in fruitSet)
+
+# Check if 'apple' is present or not in the set
+print('apple' not in fruitSet)
+
+# Change Item : Once set is created it is not possible to change its item but it can possible to add
