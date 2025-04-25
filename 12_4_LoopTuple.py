10860813820d683c3eabd0211b03a0e562861e50
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-25 13:30:28 +0600

    Loop Tuple

---
+++ 12_4_LoopTuple.py
@@ -0,0 +1,17 @@
+#### Loop Tuple ####
+
+# Itarate Through the Items and Print the Value
+fruits = ('apple', 'banana', 'cherry')
+for x in fruits:
+    print(x)
+
+# Loop Through the Index Number
+for x in range(len(fruits)):
+    print(fruits[x])
+
+# Using a while loop
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i += 1
+
