3f3e05a866aca652975cafdb7ec90229a6abc7ea
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-25 14:34:48 +0600

    Tuple Methods

---
+++ 12_06_TupleMethods.py
@@ -0,0 +1,8 @@
+#### Tuple Method ####
+
+# count() method : count an item how many time present in tuple
+num = (2, 3, 4, 2, 3, 4, 2, 4, 6, 6, 1, 0, 6, 6)
+print(num.count(6))
+
+# index() method : return index of first ocurrance of an item in a tuple
+print(num.index(3))
