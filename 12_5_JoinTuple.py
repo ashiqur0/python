dcf94471a9dc8690be2788abb76840a7728b1953
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-25 14:28:51 +0600

    Join Tuple

---
+++ 12_5_JoinTuple.py
@@ -0,0 +1,11 @@
+#### Join Tuple ####
+
+# Join Two Tuple
+tuple1 = ('a', 'b', 'c')
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# Multiply Tuple
+tuple3 = tuple1 * 2
+print(tuple3)       # Multiply The Tuple Elements
