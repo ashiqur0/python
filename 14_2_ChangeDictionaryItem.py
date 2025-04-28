ff28f0bbe7577cda809f4c01769442d12a57b93c
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-28 07:38:26 +0600

    Change Dictionary Item

---
+++ 14_2_ChangeDictionaryItem.py
@@ -0,0 +1,15 @@
+#### Change Dictionary Item ####
+
+# Change Value
+car = {
+    'brand': 'ford',
+    'model': 'mustang', 
+    'year': 1964
+}
+
+car['year'] = 2020
+print(car)
+
+# Update Dictionary
+car.update({'year': 2022})
+print(car)
