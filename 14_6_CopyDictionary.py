4f29279de2dc289e62d42f10409ad00029f121f3
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-28 08:08:34 +0600

    Copy Dictionary

---
+++ 14_6_CopyDictionary.py
@@ -0,0 +1,15 @@
+#### Copy Dictionary ####
+
+car = {
+    'brand': 'ford',
+    'model': 'mustang',
+    'year': 1964
+}
+
+# Copy a Dictionary by using copy() metod
+car1 = car.copy()
+print(car1)
+
+# Copy a Dictionay by usign dict() method
+car2 = dict(car)
+print(car2)
