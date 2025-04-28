58424df143de31ad03ad917eaf278386bdf5f2ff
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-04-28 07:45:51 +0600

    Add Dictionary Item

---
+++ 14_3_AddDictionaryItme.py
@@ -0,0 +1,15 @@
+#### Add Dictionary Itme ####
+
+# Add Items 
+car = {
+    'brand': 'ford',
+    'model': 'mustang',
+    'year': 1964
+}
+
+car['color'] = 'red'
+print(car)
+
+# Update Dictionary
+car.update({'price': "$100K"})
+print(car)
